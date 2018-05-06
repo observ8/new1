2f79e2354dee2344da62addab8d7983da21691e6
observ8 <o6serv8@gmail.com>
2018-05-07 00:38:59 +0300

    to do list ready with some bugs

--- test.js
+++ test.js
@@ -1,3 +1,42 @@
 
-x = ath.floor(Math.random() * (10 - 2) + 2);
-console.log(x)
+// x = Math.floor(Math.random() * (10 - 2) + 2);
+// §console.log(x)
+
+// let myobj = {
+//     team : "panatha",
+//     champs : 12,
+//     cups : 12,
+//     made : 1908,
+//     news : function() {
+//         console.log(this.team + " " + this.champs); 
+//      }
+// }
+
+// let newobj = Object.create(myobj)
+// // newobj.news();
+// newobj.fucker = "my fuckkk";
+// console.log(newobj.made);
+
+   
+
+// for (const key  in myobj) {
+//     // object[key];
+//    console.log(key + " = " + myobj[key] );
+   
+    
+// }
+// console.log("ooo");
+
+//  console.log(myobj.team);
+
+
+let myobj = {
+    team : "panatha",
+    champs : 12,
+    cups : 12,
+    made : 1908
+  
+}
+
+let serobj = JSON.stringify(myobj);
+console.log(serobj);
